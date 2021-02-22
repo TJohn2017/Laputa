4be28041d9400e97c54ff341f6643c0148f426c8
Daniel Guillen <eliasd.gui@gmail.com>
2021-02-21 21:11:51 -0800

    Added functionality for session to select a canvas

---
+++ Laputa/Laputa/View/SessionPage/SessionPageInputCanvas.swift
@@ -0,0 +1,59 @@
+//
+//  SessionPageInputCanvas.swift
+//  Laputa
+//
+//  Created by Daniel Guillen on 2/21/21.
+//
+
+import SwiftUI
+
+struct SessionPageInputCanvas: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(
+        entity: Item.entity(),
+        sortDescriptors: [],
+        predicate: NSPredicate(format: "id >= 0")
+    )
+    var canvases: FetchedResults<Item>
+    
+    @State var canvas: Item?
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                ForEach(canvases) { elem in
+                    Button(action: {
+                        self.canvas = elem
+                    }) {
+                        Text("Canvas \(elem.id)")
+                            .font(.title)
+                            .fontWeight(.regular)
+                            .frame(width: 400.0, height: 200.0)
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(Color.black)
+                            .cornerRadius(10.0)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct SessionPageInputCanvas_Previews: PreviewProvider {
+    static var previews: some View {
+        PreviewWrapper()
+    }
+    
+    struct PreviewWrapper: View {
+        @State var canvas: Item?
+        
+        var body: some View {
+            
+            return SessionPageInputCanvas(
+                canvas: canvas
+            ).environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        }
+    }
+}
