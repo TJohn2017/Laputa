abd5f8c20b3ef18800435738a044bdfa72ee2155
Cynthia Jia <cynthiaj@stanford.edu>
2021-02-28 18:35:50 -0500

    make cards deletable

--- Laputa/Laputa/View/Canvas/CodeCardView.swift
+++ Laputa/Laputa/View/Canvas/CodeCardView.swift
@@ -10,6 +10,8 @@ import CoreData
 
 struct CodeCardView: View {
     var codeCard : CodeCard
+    // Should be one higher than the largest zIndex available in the
+    // stack of code cards
     @Binding var maxZIndex: Double
     
     @Environment(\.managedObjectContext) private var viewContext
@@ -17,30 +19,20 @@ struct CodeCardView: View {
     // set up interaction states for conditional logic later.
     enum DragState {
         case inactive // no interaction
-        case pressing // long press in progress
         case dragging(translation: CGSize) // dragging to a new position
         
         var translation: CGSize {
             switch self {
-            case .inactive, .pressing:
+            case .inactive:
                 return .zero
             case .dragging(let translation):
                 return translation
             }
         }
         
-        var isActive: Bool {
-            switch self {
-            case .inactive:
-                return false
-            case .pressing, .dragging:
-                return true
-            }
-        }
-        
         var isDragging: Bool {
             switch self {
-            case .inactive, .pressing:
+            case .inactive:
                 return false
             case .dragging:
                 return true
@@ -50,79 +42,124 @@ struct CodeCardView: View {
     
     @GestureState var dragState = DragState.inactive
     @State var viewState = CGSize.zero
+    // Whether or not a user is currently able to delete a card
+    @State var deleting = false
+    
+    // Sets initial card position saved in CoreData
+    func setInitialOffset() {
+        self.viewState.width = CGFloat(codeCard.locX)
+        self.viewState.height = CGFloat(codeCard.locY)
+    }
+    
+    // Sets code card zIndex to the max, bringing
+    // it to the top, and saves to CoreData
+    func updateCardZIndex() {
+        viewContext.performAndWait {
+            codeCard.zIndex = maxZIndex
+            try? viewContext.save()
+        }
+    }
+    
+    // Saves card position in CoreData
+    func updateCardPosition() {
+        let newLocX = Double(viewState.width)
+        let newLocY = Double(viewState.height)
+        
+        viewContext.performAndWait {
+            codeCard.locX = newLocX
+            codeCard.locY = newLocY
+            try? viewContext.save()
+        }
+    }
+    
+    // After pressing for min duration,
+    // a button will appear to delete card
+    var longPressDelete: some Gesture {
+        LongPressGesture(minimumDuration: 0.5)
+            .onChanged { _ in
+                updateCardZIndex()
+            }
+            .onEnded { finished in
+                if finished {
+                    self.deleting.toggle()
+                }
+            }
+    }
+    
+    
+    // Used for when user taps the background to
+    // dismiss the delete button
+    var dismissDelete: some Gesture {
+        TapGesture()
+            .onEnded { _ in
+                deleting = false
+            }
+    }
     
     var body: some View {
-
-        let minimumLongPressDuration = 0.05
-        let longPressDrag = LongPressGesture(minimumDuration: minimumLongPressDuration)
-            .sequenced(before: DragGesture())
+        
+        let drag = DragGesture()
             .onChanged { _ in
                 updateCardZIndex()
             }
             .updating($dragState) { value, state, transaction in
-                switch value {
-                // Long press begins.
-                case .first(true):
-                    state = .pressing
-                // Long press confirmed, dragging may begin.
-                case .second(true, let drag):
-                    state = .dragging(translation: drag?.translation ?? .zero)
-                // Dragging ended or the long press cancelled.
-                default:
-                    state = .inactive
-                }
+                state = .dragging(translation: value.translation)
             }
             .onEnded { value in
-                guard case .second(true, let drag?) = value else { return }
-                self.viewState.width += drag.translation.width
-                self.viewState.height += drag.translation.height
+                self.viewState.width += value.translation.width
+                self.viewState.height += value.translation.height
                 maxZIndex += 1
                 
                 updateCardPosition()
             }
         
-        func updateCardPosition() {
-            let newLocX = Double(viewState.width)
-            let newLocY = Double(viewState.height)
-            
-            viewContext.performAndWait {
-                codeCard.locX = newLocX
-                codeCard.locY = newLocY
-                try? viewContext.save()
+        return
+            ZStack {
+                // A transparent rectangle to highlight card to delete.
+                // If tapped, it will dismiss the delete option.
+                if self.deleting {
+                    Rectangle()
+                        .ignoresSafeArea()
+                        .foregroundColor(Color(white: 1).opacity(0.5))
+                        .gesture(dismissDelete)
+                }
+                HStack() {
+                    Text(codeCard.wrappedText)
+                        .padding(20)
+                        .border(self.deleting ? .red : Color.white)
+                        .foregroundColor(.white)
+                        .background(Color(red: 0.2, green: 0.2, blue: 0.2, opacity: 1.0))
+                        .font(.custom("Menlo-Regular", size: 12))
+                    if self.deleting {
+                        Button(action: {
+                            viewContext.delete(codeCard)
+                            do {
+                                try viewContext.save()
+                                print("Code card deleted.")
+                            } catch {
+                                print(error.localizedDescription)
+                            }
+                        }) {
+                            Text("Delete")
+                                .padding()
+                                .foregroundColor(Color.white)
+                                .background(Color.red.opacity(0.9))
+                                .cornerRadius(10)
+                        }
+                    }
+                }
+                .offset(
+                    x: CGFloat(codeCard.locX) + dragState.translation.width,
+                    y: CGFloat(codeCard.locY) + dragState.translation.height
+                )
+                .gesture(longPressDelete)
+                // don't allow dragging while delete button is visible
+                .gesture(self.deleting ? nil : drag)
+                .shadow(radius: dragState.isDragging ? 8 : 0)
+                .onAppear(perform: setInitialOffset)
             }
-        }
-        
-        func updateCardZIndex() {
-            viewContext.performAndWait {
-                codeCard.zIndex = maxZIndex
-                try? viewContext.save()
-            }
-        }
-        
-        func setInitialOffset() {
-            self.viewState.width = CGFloat(codeCard.locX)
-            self.viewState.height = CGFloat(codeCard.locY)
-        }
-        
-        return ZStack{
-            Text(codeCard.wrappedText)
-                .padding(20)
-                .border(Color.white)
-                .foregroundColor(.white)
-                .background(Color(red: 0.2, green: 0.2, blue: 0.2, opacity: 1.0))
-                .font(.custom("Menlo-Regular", size: 12))
-        }
-        .offset(
-            x: CGFloat(codeCard.locX) + dragState.translation.width,
-            y: CGFloat(codeCard.locY) + dragState.translation.height
-        )
-        .shadow(radius: dragState.isActive ? 8 : 0)
-        .animation(.linear(duration: minimumLongPressDuration))
-        .gesture(longPressDrag)
-        .onAppear(perform: setInitialOffset)
-        .zIndex(codeCard.zIndex)
+            .zIndex(codeCard.zIndex)
     }
-    
 }
 
 struct CodeCardView_Previews: PreviewProvider {
