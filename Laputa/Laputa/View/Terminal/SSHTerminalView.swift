65ebdf027b203246f12c0394aff79da54520ec5b
TJohn2017 <TJohn2017@gmail.com>
2021-03-10 21:12:39 -0600

    Brought back some lost code for tracking last output so that we can use it for brainstorming.

--- Laputa/Laputa/View/Terminal/SSHTerminalView.swift
+++ Laputa/Laputa/View/Terminal/SSHTerminalView.swift
@@ -16,6 +16,9 @@ public class SSHTerminalView: TerminalView, TerminalViewDelegate, NMSSHChannelDe
     // Class variables
     var ssh_session: SSHConnection?
     var command_buffer = [UInt8]()
+    // TODO TJ take these out if we can't use them
+//    var shouldCatchResponse: Bool = false
+//    var lastResponse: String = ""
     
     init(connection: SSHConnection?, frame: CGRect) {
         super.init(frame: frame) // init function of TerminalView
@@ -37,6 +40,20 @@ public class SSHTerminalView: TerminalView, TerminalViewDelegate, NMSSHChannelDe
     // Async function every time data is available to be displayed on terminal
     public func channel(_ channel: NMSSHChannel, didReadData message: String) {
         print("DATA RECEIVED: \(message)")
+        
+        // TODO TJ take this out if we can't figure it out
+        // This message is a response from a command. Stash it so that the UI can work with it.
+//        if (shouldCatchResponse) {
+//            shouldCatchResponse = false
+//            print("CATCHING RESPONSE: \(message)")
+//            lastResponse = message
+//        }
+                
+        // TODO TJ: this check for the return character isn't working. It seems like it goes through send
+        //          but doesn't actually show up in didReadData so for now I just put it there
+//        if (message.count > 0 && message.last! == Character("\r")) {
+//            shouldCatchResponse = true
+//        }
         
         DispatchQueue.main.sync {
             self.feed(text: message)
@@ -75,11 +92,11 @@ public class SSHTerminalView: TerminalView, TerminalViewDelegate, NMSSHChannelDe
             // response for potential UI use.
             // TODO TJ: should we try to be more precise than this? for example, we are going to unnecessarily
             //          catch a ton of responses when doing things like using vim
+//            let lastIndex = ((data.endIndex - 1) > 0) ? (data.endIndex - 1) : 0 // Don't let last index go out of bounds
+//            if (data[lastIndex] == ReturnControlCode) {
+//                shouldCatchResponse = true
+//            }
             
-            let lastIndex = ((data.endIndex - 1) > 0) ? (data.endIndex - 1) : 0 // Don't let last index go out of bounds
-            if (data[lastIndex] == ReturnControlCode) {
-                shouldCatchResponse = true
-            }
             try ssh_session?.write(data: data)
         } catch {
             // TODO TJ figure out what error types we need to account for here
@@ -88,7 +105,7 @@ public class SSHTerminalView: TerminalView, TerminalViewDelegate, NMSSHChannelDe
     
     // Getter function that returns teh last response instance variable
     public func lastResponse() -> String {
-        return ssh_session.session.channel.lastResponse ?? ""
+        return ssh_session?.session.channel.lastResponse ?? ""
     }
     
     public func isConnected() -> Bool {
