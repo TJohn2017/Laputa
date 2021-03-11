78ef72605982186cb9ece69feb892f8fda871148
claire <clairemai4@gmail.com>
2021-03-11 13:19:44 -1000

    Got text to wrap and persist when resizing code cards and deleted useless resizing code in cordCardTermianl

--- Laputa/Laputa/View/CodeCardTerminal/CodeCardTerminal.swift
+++ Laputa/Laputa/View/CodeCardTerminal/CodeCardTerminal.swift
@@ -19,8 +19,6 @@ public class CodeCardTerminalView: TerminalView, TerminalViewDelegate {
         super.init(frame: frame) // init function of TerminalView
         terminalDelegate = self
         
-        // Resize the terminal to take only the amount of space that is required
-        self.setOptimalSize(content: content)
         print("content: \(content)")
         // Give it the static content
         self.feed(text: content)
@@ -28,29 +26,6 @@ public class CodeCardTerminalView: TerminalView, TerminalViewDelegate {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    // TODO TJ comment
-    private func setOptimalSize(content: String) {
-        var numLines = 0 // Determines optimal height
-        var longestLineLenth = 0 // Determines optimal width
-        var currLineLength = 0 // Track our current line length
-        
-        // Loop over the entire content string to find spacial attributes
-        for char in content {
-            if (char == "\n") {
-                numLines += 1
-            } else {
-                currLineLength += 1
-                if (currLineLength > longestLineLenth) {
-                    longestLineLenth = currLineLength
-                }
-            }
-        }
-        
-        // Resize to be only the size that we need
-        let terminal = self.getTerminal()
-        terminal.resize(cols: longestLineLenth, rows: numLines)
     }
 
     // Needed for conformity to protocol
